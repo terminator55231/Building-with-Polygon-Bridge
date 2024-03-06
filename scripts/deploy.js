b911f8a00427ae3df91f6dd3416ff16f85744ee5
terminator55231 <162026081+terminator55231@users.noreply.github.com>
2024-03-06 15:51:23 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,14 +12,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-  
-  /*
-npm i
-npm install dotenv --save
-npx hardhat compile 
-npx hardhat run scripts/deploy.js --network goerli
-node scripts/mint-nft.js
-node .\scripts\approveDeposit.js
-
-  */
